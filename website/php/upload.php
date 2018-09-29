1e15d43c59026a450494a4a9a6ea74581e4d0dbf
alambe22 <alambe22@vols.utk.edu>
2018-09-29 00:56:35 -0400

    Changed PHP

--- website/php/upload.php
+++ website/php/upload.php
@@ -1,17 +1,16 @@
 <?php
-function uploadFile($target){
 	//$allowed contains all allowed file types.
 	$allowed = array('py');
 	$target_dir = "uploads/";
-	$target_file = $target_dir . basename($FILES["$target"]["name"]);
+	$target_file = $target_dir . basename($FILES["fileToUpload"]["name"]);
 	$uploadOk = 1;
 	$extenType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 	if(in_array($extenType, $allowed)){
 		if(file_exists($target_file){
 			unlink($target_file);
 		}
-		if(move_uploaded_file($_FILES["$target"]["tmp_name"], $target_file)){
-			echo "The file " . basename($_FILES["$target"]["name"]). " has been uploaded.";
+		if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)){
+			echo "The file " . basename($_FILES["fileToUpload"]["name"]). " has been uploaded.";
 		}else{
 			echo "Sorry, there was an error during upload. Please try again.";
 		}
