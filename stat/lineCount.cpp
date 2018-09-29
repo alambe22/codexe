fa26ff2a9c0588ab92fea37c8e821c7a75f85926
hcoffey1 <hcoffey1@vols.utk.edu>
2018-09-29 17:42:53 -0400

    Added code to calculate line number #6

---
+++ stat/lineCount.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stdio.h>
+#include <fstream>
+#include <string>
+
+int count_lines(std::string);
+
+int main(){
+    std::string line;
+    while(std::cin >> line)
+    {
+        //std::cout << line << std::endl;
+        //std::cout << line.substr(line.find_last_of(".")+1) << std::endl;
+        if(line.substr(line.find_last_of(".")+1) == "py")
+        {
+            std::cout << line << ": " << count_lines(line) << std::endl;
+        }
+    }
+}
+
+
+int count_lines(std::string file_name)
+{
+    std::ifstream fin;
+    std::string line;
+    int line_count = 0;
+
+    fin.open(file_name);
+    if(fin.fail())
+    {
+        std::cerr << file_name << " not found.\n";
+        return -1;
+    }
+
+   while(std::getline(fin, line)) 
+       line_count++;
+
+   return line_count;
+}
