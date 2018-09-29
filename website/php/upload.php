bdacad5d1b21b0e08f939779a54b2f157c250760
alambe22 <alambe22@vols.utk.edu>
2018-09-29 00:39:04 -0400

    Base PHP written

--- website/php/upload.php
+++ website/php/upload.php
@@ -5,5 +5,16 @@ function uploadFile($target){
 	$target_dir = "uploads/";
 	$target_file = $target_dir . basename($FILES["$target"]["name"]);
 	$uploadOk = 1;
-	$
+	$extenType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+	if(in_array($extenType, $allowed)){
+		if(file_exists($target_file){
+			unlink($target_file);
+		}
+		if(move_uploaded_file($_FILES["$target"]["tmp_name"], $target_file)){
+			echo "The file " . basename($_FILES["$target"]["name"]). " has been uploaded.";
+		}else{
+			echo "Sorry, there was an error during upload. Please try again.";
+		}
+	}
 }
+?>
