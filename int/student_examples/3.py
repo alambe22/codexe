6e66fc87ead8e9a3d4af1d19c58689c2a0c5b578
Gregory Croisdale <gtcroisdale1@gmail.com>
2018-09-29 05:29:44 -0400

    idek guys

---
+++ int/student_examples/3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import time
+import sys
+
+def delay_print(s):
+    for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.25)
+
+delay_print("Hello world! ")
--- int/test.py
+++ int/test.py
@@ -1,3 +1,50 @@
 #!/usr/bin/python3
-from subprocess import call
-call(["echo", "hello"])
+from subprocess import call, check_output
+from time import time
+
+class Executable(object):
+    def __init__(self, command, desired):
+        self.command = command
+        time_init = time()
+        self.output = self.exo()
+        self.time = time() - time_init
+        self.desired = desired
+        self.result = desired == self.output
+        self.dict = {
+            "command": self.command,
+            "output": self.output,
+            "time": self.time,
+            "desired": self.desired,
+            "result": self.result
+        }
+
+    def ex(self):
+        """executes string as command without output"""
+        call(self.command.split(" "))
+
+    def exo(self):
+        """executes string as command with output"""
+        return check_output(self.command.split(" ")).decode("utf-8")[:-1]
+
+
+
+def read(url):
+    """reads string as url and returns file contents"""
+    with open(url, 'r') as file:
+        return file.read()
+
+
+
+def expOut(inst, arg=""):
+    o = {}
+    for i in inst.split("\n")[:-1]:
+        k = i.split(":$:")
+        try:
+            o[k[0]] = Executable(k[0] + " " + arg, k[1]).dict
+        except IndexError:
+            raise TypeError("Instruction file incorrectly formatted")
+    return o
+
+
+
+print(expOut(read("commands"), "'Hello world!'"))
