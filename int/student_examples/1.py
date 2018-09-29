6dd1a407a5dfe66673a3471004d8627c81c9bf9e
Gregory Croisdale <gtcroisdale1@gmail.com>
2018-09-29 05:42:43 -0400

    more features and better

--- int/student_examples/1.py
+++ int/student_examples/1.py
@@ -2,4 +2,4 @@
 
 import sys
 
-print(sys.argv[1])
+print(sys.argv[1] + " "  + sys.argv[2])
--- int/test.py
+++ int/test.py
@@ -6,10 +6,14 @@ class Executable(object):
     def __init__(self, command, desired):
         self.command = command
         time_init = time()
-        self.output = self.exo()
+        try:
+            self.output = self.exo()
+        except Exception as e:
+            if Exception == KeyboardInterrupt: raise
+            self.output = e
         self.time = time() - time_init
         self.desired = desired
-        self.result = desired == self.output
+        self.result = self.output in desired
         self.dict = {
             "command": self.command,
             "output": self.output,
@@ -24,7 +28,7 @@ class Executable(object):
 
     def exo(self):
         """executes string as command with output"""
-        return check_output(self.command.split(" ")).decode("utf-8")[:-1]
+        return check_output(self.command.split(" ")).decode("utf-8").rstrip()
 
 
 
@@ -40,11 +44,11 @@ def expOut(inst, arg=""):
     for i in inst.split("\n")[:-1]:
         k = i.split(":$:")
         try:
-            o[k[0]] = Executable(k[0] + " " + arg, k[1]).dict
+            o[k[0]] = Executable(k[0] + " " + arg, k[1].split("|:|")).dict
         except IndexError:
             raise TypeError("Instruction file incorrectly formatted")
     return o
 
 
 
-print(expOut(read("commands"), "'Hello world!'"))
+print(expOut(read("commands"), "Hello world!"))
