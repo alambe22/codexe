c3422508a4e52c16300b47490b751c0407866549
alambe22 <alambe22@vols.utk.edu>
2018-09-29 21:47:44 -0400

    PHp

--- website/php/upload.php
+++ website/php/upload.php
@@ -1,16 +1,17 @@
-
 <?php
 	//$allowed contains all allowed file types.
+	
 	$target_dir = "/var/www/html/";
 	$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 	$uploadOk = 1;
 	$extenType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-//	if(in_array($extenType, $allowed)){
+	if($extenType == "txt"){
 		if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)){
 			echo "The file " . basename($_FILES["fileToUpload"]["name"]). " has been uploaded.";
+			include("../index.html");
 		}else{
 			echo "Sorry, there was an error during upload. Please try again.";
 		}
-//	}
+	}
 
 ?>
