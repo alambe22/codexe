fdf40861ecb4670a304a68ad5c21477e355b3065
Gregory Croisdale <gtcroisdale1@gmail.com>
2018-09-30 00:50:01 -0400

    adssdfdfdgadfg

--- int/test.py
+++ int/test.py
@@ -69,7 +69,7 @@ def runCommands(cList):
             libraries = []
             libraries += re.findall(r'"(.*?)(?<!\\)"', i)
             report[j[0]] = {
-                    "language": = j[-1],
+                    "language": j[-1],
                     "lines": j[1],
                     "comments": j[2],
                     "libraries": libraries,
