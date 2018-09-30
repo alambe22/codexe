a7a43e54958febeb7f739122ab4212a518dc5788
alambe22 <alambe22@vols.utk.edu>
2018-09-30 01:58:41 -0400

    Big update

--- website/php/upload.php
+++ website/php/upload.php
@@ -1,16 +1,15 @@
 <?php
 	//$allowed contains all allowed file types.
-	$target_dir = "/var/www/html/";
-	$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-	$uploadOk = 1;
-	$extenType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
-	if($extenType == "txt"){
-		if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)){
-			echo "The file " . basename($_FILES["fileToUpload"]["name"]). " has been uploaded.";
-			include("../index.html");
-		}else{
-			echo "Sorry, there was an error during upload. Please try again.";
+	function upload($class, $assignment){
+		$target_dir = "/var/www/html/";
+		$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+		$extenType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+		if($extenType == "txt"){
+			if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)){
+				echo "The file " . basename($_FILES["fileToUpload"]["name"]). " has been uploaded.";
+			}else{
+				echo "Sorry, there was an error during upload. Please try again.";
+			}
 		}
 	}
-	
 ?>
