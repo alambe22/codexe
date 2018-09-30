9577679ae492f686b9645fcadd83d954bd23dd1c
Gregory Croisdale <gtcroisdale1@gmail.com>
2018-09-29 20:26:28 -0400

    asddfsbdfgb

--- int/test.py
+++ int/test.py
@@ -51,7 +51,6 @@ def runCommands(cList):
             args = text[0]
             expected = text[1:]
             for i in read("images").rstrip().split("\n"):
-                print(i + " HEY HEY HEY")
                 k = Executable(i + " " + args, expected)
                 o += [k.dict]
     return o
