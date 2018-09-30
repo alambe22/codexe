d2bc90430f0fc9696b47133bd068d3309af2a2bd
Gregory Croisdale <gtcroisdale1@gmail.com>
2018-09-30 06:14:51 -0400

    json visualizer

---
+++ int/vis.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import json
+import sys
+
+def getJ(url):
+    with open(url, "r") as read_file:
+        return json.load(read_file)
+
+def it(test):
+    o = []
+    if isinstance(test, dict):
+        p = []
+        for i in test.keys():
+            o += [i, it(test[i])]
+        o += p
+    else:
+        return [test]
+    return o
+
+def htmlize(g):
+    o = "<ul>"
+    if isinstance(g, list):
+        for i in g:
+            o += htmlize(i)
+        return o + "</ul>"
+    else:
+        return "<li>" + str(g).replace("<","&lt;".replace(">","&gt")) + "</li>"
+
+def out(s, u):
+    f = open(u, "w")
+    f.write(s)
+
+y = getJ(sys.argv[1])
+out(htmlize(it(y)),"json.html")
